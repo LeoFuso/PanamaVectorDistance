c5588f13891954572f7f9a558c1413a08492efb3
Leonardo Nuzzo <leonardo.nuzzo@ifood.com.br>
2022-06-17 00:00:15 -0300

    main – Updating JExtract script

--- jextract.sh
+++ jextract.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd src || exit
-jextract --source -t org.distance -ldistance__m256d -I /usr/local/include /usr/local/include/distance__m256d.h
+jextract --source -t org.distance -ldistance__m256d \
+                  -I /usr/local/include /usr/local/include/distance__m256d.h \
+                  --include-function euclidean \
+                  --include-function manhattan \
+                  --include-function cosine \
+                  --include-function aligned_alloc \
+                  --dump-includes=includes.txt
+
 cd ..
